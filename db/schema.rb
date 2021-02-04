7e004ea3a3d9b54b7864291aec1ae6d35a9ef870
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-04 12:48:18 +0900

    booksテーブルに画像pictureカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_023524) do
+ActiveRecord::Schema.define(version: 2021_02_04_025832) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_02_04_023524) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
+    t.string "picture"
   end
 
 end
