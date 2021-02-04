1e4860e6f9a6a087104308cf8edf9aaf51037950
ToshiroKoba <t-koba@ruby-dev.jp>
2021-02-04 11:41:52 +0900

    booksテーブルに著者authorカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_123326) do
+ActiveRecord::Schema.define(version: 2021_02_04_023524) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
   end
 
 end
